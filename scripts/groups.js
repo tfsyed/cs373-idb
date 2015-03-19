23afce6b18b9d23f0183fb7671003c7d293969fd
Aizhuldyz Gabbassova <aigabbass90@gmail.com>
2015-03-19 18:28:32 -0500

    Static page with team members

---
+++ scripts/groups.js
@@ -0,0 +1,27 @@
+var jumboHeight = $('.jumbotron').outerHeight();
+        function parallax(){
+            var scrolled = $(window).scrollTop();
+            $('.bg').css('height', (jumboHeight-scrolled) + 'px');
+        }
+
+        $(window).scroll(function(e){
+            parallax();
+        });
+
+
+// Sidebar stuff
+$('#sidebar').affix({
+      offset: {
+        top: 245
+      }
+});
+
+var $body   = $(document.body);
+var navHeight = $('.navbar').outerHeight(true) + 10;
+
+$body.scrollspy({
+	target: '#leftCol',
+	offset: navHeight
+});/**
+ * Created by aizhuldyzgabbassova on 3/12/15.
+ */
